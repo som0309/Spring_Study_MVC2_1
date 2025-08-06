e3c4bad6843830c04cdd1d647919c69ae1512ea2
eunseolim <eunseolim1018@gmail.com>
2025-08-06 14:42:15 +0900

    mvc_section5,6

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,36 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class Item {
+
+//    @NotNull(groups = UpdateCheck.class) //수정 시에만 적용
+    private Long id;
+
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    private String itemName;
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+    private Integer price;
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class) //등록 시에만 적용
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
